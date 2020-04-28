52630b51cdf3427c75d1f42e7502a9a4d0ae8e5e
autumn-ragland <raglanda998@gmail.com>
2020-04-28 16:30:24 -0500

    update schema name

--- server/models/CommentSchema.js
+++ server/models/CommentSchema.js
@@ -1,11 +1,11 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-let UserSchema = new Schema (
+let CommentSchema = new Schema (
     {
         commentTitle : {required : true, type : String},
         commentsBody : {required : true, type : String},
     }
 );
 
-module.exports = mongoose.model('commentsCW200428', UserSchema);
+module.exports = mongoose.model('commentsCW200428', CommentSchema);
